5499de7fb118b9d2eb0fb0da1d3f57b74c98521e
Sean McNair <smcnair@annie-mac.com>
2021-12-31 00:55:53 -0500

    build out shop page with info modals.  Cart Buttons do nothing

---
+++ js/shopScript.js
@@ -0,0 +1,12 @@
+    $("#quickDrawSpecs").click(function() {
+        $("#quickDrawModal").modal("show");
+    });
+    $("#ropeRugSpecs").click(function() {
+        $("#ropeRugModal").modal("show");
+    });
+    $("#crackKeySpecs").click(function() {
+        $("#crackKeyModal").modal("show");
+    });
+    $("#chalkBagSpecs").click(function() {
+        $("#chalkBagModal").modal("show");
+    });
